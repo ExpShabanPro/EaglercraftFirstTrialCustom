637e2c05005a36546981150b115ff6e7368631bc
Exp Mohammad Shaban <122181141+ExpShabanPro@users.noreply.github.com>
2026-03-14 17:27:10 -0600

    Enhance Timer.java with comments for clarity
    Added comments to clarify tickLength and getPartialTicks method.

--- teavm-test/src/main/java/net/minecraft/util/Timer.java
+++ teavm-test/src/main/java/net/minecraft/util/Timer.java
@@ -11,16 +11,25 @@ public class Timer {
    private final float tickLength;
 
    public Timer(float ticks, long lastSyncSysClock) {
+      // tickLength is usually 50.0ms for 20 TPS
       this.tickLength = 1000.0F / ticks;
       this.lastSyncSysClock = lastSyncSysClock;
    }
 
+   /**
+    * Updated for Web: This method calculates how many logical ticks have passed 
+    * since the last frame. In the browser, this helps keep movement smooth
+    * even if the framerate (FPS) varies.
+    */
    public int getPartialTicks(long gameTime) {
+      // Calculate the time difference using the browser's millisecond clock
       this.elapsedPartialTicks = (float)(gameTime - this.lastSyncSysClock) / this.tickLength;
       this.lastSyncSysClock = gameTime;
       this.renderPartialTicks += this.elapsedPartialTicks;
+      
       int i = (int)this.renderPartialTicks;
       this.renderPartialTicks -= (float)i;
+      
       return i;
    }
 }
